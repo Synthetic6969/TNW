89adcebb5453f1f71b5eadd62cdd0d10b8977216
Synthetic6969 <69759521+Synthetic6969@users.noreply.github.com>
2021-01-25 19:33:46 +0000

    Create modLog.lua

---
+++ modLog.lua
@@ -0,0 +1,39 @@
+local gmt = getrawmetatable(game)
+local oldNamecall = gmt.__namecall
+setreadonly(gmt, false)
+
+local s = ""
+for i = 1,10 do
+    s = s..string.char(math.random(1, 200))
+end
+getgenv().kickString = s
+
+gmt.__namecall = newcclosure(function(self, ...)
+    local args = {...}
+    local method = getnamecallmethod()
+    if string.lower(tostring(method)) == "kick" and self == player then
+        if args[1] == getgenv().kickString then
+            return oldNamecall(self, "Moderator Detected")
+        else
+            return
+        end
+    end
+    return oldNamecall(self, ...)
+end)
+
+while true do
+    for _, v in next, game:service'Players':GetPlayers() do
+        if v:GetRoleInGroup(6867395) ~= "Fan" then
+            if getgenv().ModLog then
+                player:kick(getgenv().kickString)
+            else
+                game:service'StarterGui':SetCore("SendNotification", {
+                    Title = "Moderator Detected",
+                    Text = v.Name,
+                    Button1 = "what a whitehat"
+                })
+            end
+        end
+    end
+    wait(.5)
+end
